e99d56d7a641c4e1e5b00fc994e81e0cb415cd9b
Hanno Schlichting <hanno@jarn.com>
2008-01-25 09:24:02 +0000

    Added compatibility with Zope 2.10 / Plone 3.0.

--- plone/reload/patches.py
+++ plone/reload/patches.py
@@ -1,11 +1,28 @@
-from Products.GenericSetup import zcml
-from wicked.fieldevent import meta
+# Conditional imports to make it work when those packages are not available
 
 def patch_generic_setup():
-    zcml.cleanUpProfiles()
-    zcml.cleanUpImportSteps()
-    zcml.cleanUpExportSteps()
+
+    # GS 1.3
+    try:
+        from Products.GenericSetup.zcml import cleanUp
+        cleanUp()
+    except ImportError:
+        pass
+    # GS 1.4
+    try:
+        from Products.GenericSetup.zcml import cleanUpProfiles
+        from Products.GenericSetup.zcml import cleanUpImportSteps
+        from Products.GenericSetup.zcml import cleanUpExportSteps
+        cleanUpProfiles()
+        cleanUpImportSteps()
+        cleanUpExportSteps()
+    except ImportError:
+        pass
 
 
 def patch_wicked():
-    meta.cleanUp()
+    try:
+        from wicked.fieldevent.meta import cleanUp
+        cleanUp()
+    except ImportError:
+        pass
