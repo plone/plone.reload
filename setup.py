317b675db989fb5c9eb9dbeab816ac402adf3624
Hanno Schlichting <hanno@jarn.com>
2008-01-25 00:18:56 +0000

    Bump version to 0.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 
 name = 'plone.reload'
-version = '0.1'
+version = '0.2'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
