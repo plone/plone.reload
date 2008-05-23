2a3a67a9094ac9ebaa5a77ebf190b128b24d09fc
Hanno Schlichting <hanno@jarn.com>
2008-05-23 20:43:33 +0000

    Removed various levels of caching which turned out to be premature and caused lots of modules not to be found. Thx to optilude for reminding me of this project ;)

--- plone/reload/code.py
+++ plone/reload/code.py
@@ -14,9 +14,8 @@ _marker = object()
 BLACKLIST = _marker
 EXCLUDE_EGGS = True
 EXCLUDE_SITE_PACKAGES = True
-MOD_TIMES = _marker
-SEARCH_MODULES = _marker
-SEARCH_PATH = _marker
+MOD_TIMES = dict()
+
 
 def blacklist():
     global BLACKLIST
@@ -28,63 +27,51 @@ def blacklist():
 
         if 'plone.reload' not in sys.modules:
             import plone.reload
+        # Get the base dir and not the plone/reload package
         pr = dirname(sys.modules['plone.reload'].__file__)
-
+        pr = abspath(os.path.join(pr, '..', '..'))
         BLACKLIST = frozenset([abspath(p) for p in
                               (tmp, swhome, zopehome, pyhome, pr) if p])
 
     return BLACKLIST
 
 
-def search_path():
-    global SEARCH_PATH
-    if SEARCH_PATH is _marker:
-        path = []
-        sys_path = [p for p in tuple(sys.path) if p]
-        if EXCLUDE_SITE_PACKAGES:
-            sys_path = [p for p in sys_path if 'site-packages' not in p]
-        if EXCLUDE_SITE_PACKAGES:
-            sys_path = [p for p in sys_path if '.egg' not in p]
-
-        for p in sys_path:
-            if True not in [abspath(p).startswith(b) for b in blacklist()]:
-                path.append(p)
-
-        SEARCH_PATH = frozenset(path)
-
-    return SEARCH_PATH
-
-
 def in_search_path(path):
-    for p in search_path():
-        if path.startswith(p):
-            return True
-    return False
+    if EXCLUDE_SITE_PACKAGES:
+        if 'site-packages' in path:
+            return False
+        elif '.egg' in path:
+            return False
+
+    if True in [abspath(path).startswith(b) for b in blacklist()]:
+        return False
+
+    return True
 
 
 def search_modules():
-    global SEARCH_MODULES
-    if SEARCH_MODULES is _marker:
-        modules = []
-        for name, module in sys.modules.items():
-            if module is not None:
-                f = getattr(module, '__file__', None)
-                if f is None:
-                    continue
-                f = abspath(f)
-                if in_search_path(f):
-                    modules.append((f, module))
-        SEARCH_MODULES = frozenset(modules)
-    return SEARCH_MODULES
+    modules = []
+    for name, module in sys.modules.items():
+        if module is not None:
+            f = getattr(module, '__file__', None)
+            # Standard library modules don't have a __file__
+            if f is None:
+                continue
+            f = abspath(f)
+            if in_search_path(f):
+                modules.append((f, module))
+    return modules
 
 
 def get_mod_time(path):
-    found = path
     mtime = 0
-    if path.endswith('pyc'):
-        if os.path.isfile(path[:-1]):
-            found = path[:-1]
-    if isfile(found):
+    # If we have the compiled source, look for the source code change date
+    # should the file exist
+    if path.endswith('pyc') or path.endswith('pyo'):
+        source = path[:-1]
+        if os.path.isfile(source):
+            path = source
+    if isfile(path):
         try:
             mtime = os.stat(path)[8]
         except (OSError, IOError):
@@ -92,18 +79,17 @@ def get_mod_time(path):
     return mtime
 
 
-def setup_mod_times(refresh=False):
+def get_mod_times():
     global MOD_TIMES
-    if MOD_TIMES is _marker or refresh:
-        MOD_TIMES = dict()
-        for path, module in search_modules():
+    for path, module in search_modules():
+        if path not in MOD_TIMES:
             MOD_TIMES[path] = (get_mod_time(path), module)
     return MOD_TIMES
 
 
 def check_mod_times():
     changed = []
-    for path, (time, module) in setup_mod_times().items():
+    for path, (time, module) in get_mod_times().items():
         newtime = get_mod_time(path)
         if time < newtime:
             changed.append((path, newtime, module))
