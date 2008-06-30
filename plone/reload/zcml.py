7c2e7f8862ca397d712e9e337f87dfa256db715c
Hanno Schlichting <hanno@jarn.com>
2008-06-30 16:40:56 +0000

    In case of errors during ZCML reload, restore the original global site manager state, instead of leaving it in a broken state. This allows you to fix and retry the ZCML reload as many times as you need.

--- plone/reload/zcml.py
+++ plone/reload/zcml.py
@@ -7,16 +7,22 @@ from plone.reload import PATCHES
 
 
 def reload_zcml():
-    setSite(None)
     gsm = getGlobalSiteManager()
-    gsm.__init__(gsm.__name__)
+    old_gsm_dict = gsm.__dict__.copy()
+    try:
+        setSite(None)
+        gsm.__init__(gsm.__name__)
+        # Apply patches
+        global PATCHES
+        for patch in PATCHES:
+            patch()
 
-    # Apply patches
-    global PATCHES
-    for patch in PATCHES:
-        patch()
-
-    # Reload all ZCML
-    zcml._initialized = False
-    zcml._context._seen_files.clear()
-    zcml.load_site()
+        # Reload all ZCML
+        zcml._initialized = False
+        zcml._context._seen_files.clear()
+        zcml.load_site()
+    except Exception, e:
+        gsm.__init__(gsm.__name__)
+        gsm.__dict__.clear()
+        gsm.__dict__.update(old_gsm_dict)
+        raise e
