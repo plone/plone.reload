05f90ec7abcf6e374163267bdf05e2efb89ab713
Martin Aspeli <optilude@gmail.com>
2009-03-22 03:50:23 +0000

    Updating version on trunk after tagging

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 
 name = 'plone.reload'
-version = '0.10'
+version = '0.11'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
