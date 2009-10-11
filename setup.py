8f8b65cd9f84a0432b60be5ec42be1bccde3000d
Hanno Schlichting <hanno@jarn.com>
2009-10-11 09:54:43 +0000

    Prepare release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import setup, find_packages
 
 name = 'plone.reload'
-version = '1.0dev'
+version = '1.0'
 
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
